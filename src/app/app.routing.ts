5c627f717e87c9bea4498656d566c03eb8360cf9
Peter Dillon <q12721@corp.cusa.canon.com>
2018-06-25 09:03:28 -0400

    Initial commit.

---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { GuardedComponent } from './guarded';
+import { LoginComponent } from './login';
+import { AuthGuard } from './_guards';
+
+const appRoutes: Routes = [
+    { path: 'guarded', component: GuardedComponent, canActivate: [AuthGuard] },
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
---
+++ src/app/guarded/guarded.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({templateUrl: 'guarded.component.html'})
+export class GuardedComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit() {
+        
+    }
+}
